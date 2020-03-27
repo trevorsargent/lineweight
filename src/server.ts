b069bc29872d5de24a56970ad0650faab7b782d9
Trevor Sargent <trevor@lucid.rocks>
2020-03-26 17:39:03 -0700

    add discover gateway

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.get('/gateway', async (req, res) => {
+  res.send(await discoverGateway())
+})
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
