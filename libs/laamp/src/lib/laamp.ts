1d2d3acbe07fb782335dd665b80c410dcb1e5929
Trevor Sargent <trevor@lucid.rocks>
2020-05-20 10:46:46 -0700

    feat(laamp): restructure laamp lib

--- libs/laamp/src/lib/laamp.ts
+++ libs/laamp/src/lib/laamp.ts
@@ -1,3 +1,15 @@
-export function laamp(): string {
-  return 'laamp';
+import { ID } from '@lineweight/types'
+
+export interface Laamp {
+  id: ID
+  adapter: LaampAdapter
+}
+
+export interface LaampAdapter {
+  adapterType: LaampAdapterType
+}
+
+export enum LaampAdapterType {
+  TRADFRI,
+  HUE,
 }
