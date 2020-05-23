b32691582d38ad4eb584a6a5b2566dd181b4d600
Trevor Sargent <trevor@lucid.rocks>
2020-05-23 14:40:40 -0700

    style(meatman): format console logs

--- apps/meatman/src/main.ts
+++ apps/meatman/src/main.ts
@@ -4,4 +4,6 @@ import { identity, psk } from '../src/basestation.config/tradfri'
 
 const osc = eventStream({ bindingAddress: '0.0.0.0', port: 3031 })
 
-config$({ identity, psk }).subscribe((config) => console.log(config))
+config$({ identity, psk }).subscribe((config) =>
+  console.log(JSON.stringify(config, null, 2))
+)
