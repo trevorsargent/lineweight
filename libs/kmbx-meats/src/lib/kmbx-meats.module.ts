ff34b8fc6da73ba997547aec146006eb3d7d1f24
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-06-08 21:45:22 -0700

    chore(kmbx-meats): add button

--- libs/kmbx-meats/src/lib/kmbx-meats.module.ts
+++ libs/kmbx-meats/src/lib/kmbx-meats.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common';
-import { ButtonCtaComponent } from './buttons/button-cta/button-cta.component'
+import { ButtonCtaComponent } from './buttons/button-cta/button-cta.component';
+import { ButtonComponent } from './buttons/button/button.component'
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonCtaComponent],
-  exports: [ButtonCtaComponent],
+  declarations: [ButtonCtaComponent, ButtonComponent],
+  exports: [ButtonCtaComponent, ButtonComponent],
 })
 export class KmbxMeatsModule {}
