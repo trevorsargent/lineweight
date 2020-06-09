a89721b4b9dbcb8ff354880828e58933dc21eb8e
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-06-08 21:49:01 -0700

    feat(kmbx-meats): add storybook

---
+++ libs/kmbx-meats/src/lib/buttons/button-cta/button-cta.component.stories.ts
@@ -0,0 +1,15 @@
+
+import { ButtonCtaComponent } from './button-cta.component';
+
+export default {
+  title: 'ButtonCtaComponent'
+}
+
+export const primary = () => ({
+  moduleMetadata: {
+    imports: []
+  },
+  component: ButtonCtaComponent,
+  props: {
+  }
+})
