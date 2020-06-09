0b1483a6c60586cabf4c7b637e452a007f6d3b90
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-06-08 21:43:56 -0700

    chore(kmbx-meats): add button-cta

---
+++ libs/kmbx-meats/src/lib/buttons/button-cta/button-cta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'meat-button-cta',
+  templateUrl: './button-cta.component.html',
+  styleUrls: ['./button-cta.component.scss']
+})
+export class ButtonCtaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/kmbx-meats/src/lib/kmbx-meats.module.ts
+++ libs/kmbx-meats/src/lib/kmbx-meats.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
+import { CommonModule } from '@angular/common';
+import { ButtonCtaComponent } from './buttons/button-cta/button-cta.component'
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ButtonCtaComponent],
+  exports: [ButtonCtaComponent],
 })
 export class KmbxMeatsModule {}
