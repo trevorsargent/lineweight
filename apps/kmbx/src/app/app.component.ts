5fa7e6966a024c875ce233365905a39278656263
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-06-08 21:36:21 -0700

    feat(kmbx): add kmbx

---
+++ apps/kmbx/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'lineweight-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'kmbx'
+}
