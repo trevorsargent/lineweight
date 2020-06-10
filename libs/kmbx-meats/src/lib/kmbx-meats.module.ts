b154a2578cc43080b425cbeb962253eea1fa0552
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-06-09 22:00:40 -0700

    chore(kmbx-meats): remove those components

--- libs/kmbx-meats/src/lib/kmbx-meats.module.ts
+++ libs/kmbx-meats/src/lib/kmbx-meats.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { ButtonCtaComponent } from './buttons/button-cta/button-cta.component'
-import { ButtonComponent } from './buttons/button/button.component'
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonCtaComponent, ButtonComponent],
-  exports: [ButtonCtaComponent, ButtonComponent],
+  declarations: [],
+  exports: [],
 })
 export class KmbxMeatsModule {}
