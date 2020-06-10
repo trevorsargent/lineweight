c78e83abaae846f8b6175fb596b78fb2e67c2160
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-06-09 21:46:32 -0700

    lint(kmbx-meats): module ts

--- libs/kmbx-meats/src/lib/kmbx-meats.module.ts
+++ libs/kmbx-meats/src/lib/kmbx-meats.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common';
-import { ButtonCtaComponent } from './buttons/button-cta/button-cta.component';
+import { CommonModule } from '@angular/common'
+import { ButtonCtaComponent } from './buttons/button-cta/button-cta.component'
 import { ButtonComponent } from './buttons/button/button.component'
 
 @NgModule({
