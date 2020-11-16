e0678ae07319c5911a7c6ab37a9dcfd92bdbb7c6
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-11-16 12:32:48 -0800

    wip(wrl): add text and add some global styles

--- apps/weather-room-log/src/app/app-routing.module.ts
+++ apps/weather-room-log/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
+import { DownByTheRiverComponent } from './pages/down-by-the-river/down-by-the-river.component'
 import { SurfaceComponent } from './surface/surface.component'
 import { VideoTrackComponent } from './video-track/video-track.component'
 
 const routes: Routes = [
   {
     path: '',
-    component: SurfaceComponent,
+    component: DownByTheRiverComponent,
   },
 ]
 
