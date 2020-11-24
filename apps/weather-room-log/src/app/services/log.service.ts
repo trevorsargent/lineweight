b79c8b1ba8c5a508bed2fd422351e99a2bbadb6b
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-11-23 19:20:24 -0800

    wip(wrl): logging service upgrades

--- apps/weather-room-log/src/app/services/log.service.ts
+++ apps/weather-room-log/src/app/services/log.service.ts
@@ -4,14 +4,15 @@ import { AngularFireDatabase } from '@angular/fire/database'
 
 import { DateTime } from 'luxon'
 import { Observable } from 'rxjs'
+import { map, timestamp } from 'rxjs/operators'
 
 import * as uuid from 'uuid'
 import { ScheduleService } from './schedule.service'
 
 @Injectable({ providedIn: 'root' })
 export class LogService {
-  entries$: Observable<any[]>
-  private token: string
+  public actions$: Observable<ActionLog[]>
+  private userId: string
 
   private readonly COLLECTION = 'dev'
 
@@ -21,16 +22,60 @@ export class LogService {
   ) {
     const WR_TOKEN = 'wr-log-id'
 
-    this.token = localStorage.getItem(WR_TOKEN) ?? uuid.v4()
-    localStorage.setItem(WR_TOKEN, this.token)
+    this.userId = localStorage.getItem(WR_TOKEN) ?? uuid.v4()
+    localStorage.setItem(WR_TOKEN, this.userId)
+    this.actions$ = this.firestore
+      .list(this.COLLECTION)
+      .valueChanges()
+      .pipe(map((x: ActionLogData[]) => x.map(dataToAction)))
   }
 
   logAction(trackId: string) {
-    this.firestore.list(this.COLLECTION).push({
+    const action: ActionLog = {
       trackId,
-      event: this.schedule.getCurrentEvent().toISO(),
-      timestamp: DateTime.local().toISO(),
-      userId: this.token,
-    })
+      event: this.schedule.getCurrentEvent(),
+      timestamp: DateTime.local(),
+      userId: this.userId,
+    }
+
+    this.firestore.list(this.COLLECTION).push(actionToData(action))
   }
+}
+
+const actionToData = ({
+  event,
+  timestamp,
+  trackId,
+  userId,
+}: ActionLog): ActionLogData => ({
+  userId,
+  trackId,
+  timestamp: timestamp.toISO(),
+  event: event.toISO(),
+})
+
+const dataToAction = ({
+  event,
+  timestamp,
+  userId,
+  trackId,
+}: ActionLogData): ActionLog => ({
+  trackId,
+  userId,
+  timestamp: DateTime.fromISO(timestamp),
+  event: DateTime.fromISO(event),
+})
+
+export interface ActionLogData {
+  trackId: string
+  event: string
+  timestamp: string
+  userId: string
+}
+
+export interface ActionLog {
+  trackId: string
+  event: DateTime
+  timestamp: DateTime
+  userId: string
 }
