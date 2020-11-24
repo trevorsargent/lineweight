76553af0d7afa16887478ace258e592766a91a44
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-11-23 21:33:32 -0800

    wip(wrl): add captions service

--- apps/weather-room-log/src/app/app.tracks.ts
+++ apps/weather-room-log/src/app/app.tracks.ts
@@ -1,18 +1,22 @@
 import { TrackData } from './video-track/video-track.types'
-
+export enum TrackId {
+  REBECCA = 'Rebecca',
+  CRISTI = 'Cristi',
+  JACOB = 'Jacob',
+}
 export const tracks: TrackData[] = [
   {
-    id: 'Rebecca',
+    id: TrackId.REBECCA,
     src:
       'https://storage.googleapis.com/wr-down-by-the-river/20201126/REBECCA.mp4',
   },
   {
-    id: 'Cristi',
+    id: TrackId.CRISTI,
     src:
       'https://storage.googleapis.com/wr-down-by-the-river/20201126/CRISTI.mp4',
   },
   {
-    id: 'Jacob',
+    id: TrackId.JACOB,
     src:
       'https://storage.googleapis.com/wr-down-by-the-river/20201126/JACOB.mp4',
   },
--- apps/weather-room-log/src/app/services/schedule.service.ts
+++ apps/weather-room-log/src/app/services/schedule.service.ts
@@ -36,7 +36,7 @@ export class ScheduleService {
     // DateTime.local().minus(
     //   this.EVENT_DURATION.minus(Duration.fromObject({ seconds: 30 })),
     // ),
-    DateTime.local().plus(Duration.fromObject({ seconds: 5 })),
+    DateTime.local().minus(Duration.fromObject({ minutes: 4, seconds: 45 })),
   ]
 
   getNextEvent(): DateTime {
