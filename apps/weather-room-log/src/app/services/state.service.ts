1cc8b07947c259536099e259139b5f38130467ff
Trevor Sargent <trevor.a.sargent@gmail.com>
2020-11-23 17:28:42 -0800

    wip(wrl): various ui and control fixes

--- apps/weather-room-log/src/app/services/state.service.ts
+++ apps/weather-room-log/src/app/services/state.service.ts
@@ -11,41 +11,69 @@ export class StateService {
 
   private _active: boolean = false
 
-  private startHook: () => void
+  private activateHook: () => void
+  private postShowHook: () => void
+  private resetHook: () => void
 
-  registerStartHook(startHook: () => void) {
-    this.startHook = startHook
+  registerActivateHook(activateHook: () => void) {
+    this.activateHook = activateHook
+  }
+
+  registerPostShowHook(postShowHook: () => void) {
+    this.postShowHook = postShowHook
+  }
+
+  registerResetHook(resetHook: () => void) {
+    this.resetHook = resetHook
   }
 
   public notifyLoaded() {
     this._loaded = true
   }
 
-  get loaded() {
-    return this._loaded
+  get showControls() {
+    return (
+      this._loaded &&
+      this._active &&
+      this.schedule.getCurrentEventTimeCode() <
+        this.schedule.PERFORMANCE_DURATION.as('seconds')
+    )
   }
 
-  public notifyStarted() {
-    if (this.schedule.getCurrentEventTimeCode()) {
-      this.startHook()
-    } else {
-      setTimeout(
-        this.startHook,
-        this.schedule.getNextEvent().diffNow().as('milliseconds'),
-      )
-    }
-    this._started = true
+  get loaded() {
+    return this._loaded
   }
 
   get started() {
     return this._started
   }
 
+  public notifyStarted() {
+    if (this.schedule.getCurrentEventTimeCode()) {
+      this.activateHook()
+    } else {
+      setTimeout(this.activateHook, this.schedule.getTimeToNextShowMilis())
+    }
+    this._started = true
+  }
+
   public notifyActive() {
+    setTimeout(this.postShowHook, this.schedule.getTimeToPostShowMilis())
+    setTimeout(this.resetHook, this.schedule.getTimeToResetMilis())
+    setTimeout(this.reset.bind(this), this.schedule.getTimeToResetMilis())
     this._active = true
   }
 
   get active() {
     return this._active
+  }
+
+  get preshow() {
+    return !this.schedule.getCurrentEventTimeCode()
+  }
+
+  reset() {
+    this._started = false
+    this._active = false
   }
 }
